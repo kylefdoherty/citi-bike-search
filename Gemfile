3dc785b7ca75203462302ce08a3f7430c976bed5
amyrjohnson <amy.rebecca.johnson@gmail.com>
2014-07-23 21:37:41 -0400

    change database to pg

--- Gemfile
+++ Gemfile
@@ -14,10 +14,24 @@ gem "geocoder"
 gem "gon-sinatra"
 gem "rack-flash3"
 
+group :development, :test do
+
+    gem "sqlite3"
+    gem "dm-sqlite-adapter"
+
+end
+
 group :test do
   gem 'rspec'
   gem 'capybara'
   gem 'rack-test'
   gem 'selenium-webdriver'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+end
+
+group :production do
+
+    gem "pg"
+    gem "dm-postgres-adapter"
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+
+  configure :development do
+
+    DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+
+    end
+
+    configure :production do
+
+        DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_RED_URL'])
+
+    end
 end
